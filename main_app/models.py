2ce386d5566b999fd71e2f6582cd156973d86098
alexehouston <alexehouston@gmail.com>
2022-11-21 17:27:40 -0600

    make movie model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+RATINGS = (
+    ('1'),
+    ('2'),
+    ('3'),
+    ('4'),
+    ('5'),
+    ('6'),
+    ('7'),
+    ('8'),
+    ('9'),
+    ('10'),
+)
+
+class Movie(models.Model):
+    imdbId = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    genres = models.CharField(max_length=200, null=True)
+    rating = models.IntegerField(
+        # choices=RATINGS,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
