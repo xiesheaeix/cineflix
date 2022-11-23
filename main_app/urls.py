f76befddb7729bfd580f0e2525df3b5b6bc31a09
xiesheaeix <sheae123@gmail.com>
2022-11-22 16:03:27 -0800

    create user profile

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
+    path('create_profile', views.create_profile.as_view(), name='create_profile'),
     path('search/', views.search, name='search'),
     path('top_movies/', views.top_movies, name='top_movies'),
     path('coming_soon', views.coming_soon, name='coming_soon'),
